808ed1faed544aa6f1a0e72ad9867a4384d64700
fauzynurn <fnnoviansyah@gmail.com>
2020-12-15 10:01:22 +0700

    remove unused import

--- lib/ui/screen/setting/setting_controller.dart
+++ lib/ui/screen/setting/setting_controller.dart
@@ -1,5 +1,4 @@
 import 'package:android_alarm_manager/android_alarm_manager.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:restaurant_app/data/source/local/local_datasource.dart';
